63fb96338cfee8e1765909a2c9a7f6b062fa7b20
Mohammad Allam <uodeeb@gmail.com>
2023-04-19 23:51:56 +0200

    end of print function

---
+++ session01.py
@@ -0,0 +1,20 @@
+# print function is the most used function in python
+# we can use it to print anything to the console
+# for example: print a string
+print('this is a string to be printed!')
+# running the python file as usual 
+# print an integer
+print(1223)
+# print a list
+print(['test01', 'test20'])
+# the print function can also print to separate values
+print('test01', 'test02')
+# concatenate two strings
+print('test01' + 'test02')
+# concatenate a strings and an integer
+# print('test' + 123) # TypeError: can only concatenate str (not "int") to str
+# to fix that
+print('test' + str(123))
+print('test' + '123')
+print(int('456') + 123)
+
