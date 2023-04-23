d6bad7eb252816f2bf1ae58a1e21d01f7ae8f174
Mohammad Allam <uodeeb@gmail.com>
2023-04-23 18:59:52 +0200

    end input session

---
+++ session01.py
@@ -0,0 +1,11 @@
+# the input function takes input from user and return it
+user_name = input()
+print(user_name)
+
+# to help the user to type the right input, ask about the name
+name = input("What is your name?")
+print("Hello, " + name)
+
+# you may add a new line at the end of your query like this
+age = input("what's your age?\n")
+print("your age is: " + age)
