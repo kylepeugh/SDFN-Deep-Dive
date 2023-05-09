db2278dbf4906e7b736287826eeccbf3b13f5f1b
Mohammad Allam <uodeeb@gmail.com>
2023-05-09 18:10:39 +0300

    end task01

---
+++ session02.py
@@ -0,0 +1,9 @@
+# 1. Print descriptions of what's happening for the player
+print("hello player, this game is about..........")
+# a good practice is to explain your game clearly to help players
+# navigate it easly, for example
+print("You find yourself in a place") # the place may be changed to add randomness
+print("To escape, you have to choose your direction") # the options may be changed
+print("You will have an enemy to fight") # the enemy may be changed to add randomness
+print("You win if you kill the enemy")
+print("You lose if the enemy killed you")
