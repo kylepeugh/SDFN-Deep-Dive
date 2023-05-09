3a28e430972eb02a8332cede26c60b6123e06cf3
Mohammad Allam <uodeeb@gmail.com>
2023-05-09 19:48:18 +0300

    end task05

--- session02.py
+++ session02.py
@@ -1,46 +1,27 @@
-# 4. Make sure the player gives a valid input
-# user_choice = input("choose 1 or 2\n 1. Water\n 2. Tea\n")
-#     # now, to keep the game play easy and consistent, options should be like 1 or 2
-#     # to check invalid input, you have to use a conditional like this3
-# if user_choice == 1:
-#     # a problem will rise here because of the differnt datatypes
-#     # as we compare the string (user_choice) with the integer (1)
-#     # to fix this, you can  use something like str or ""
-#     print("your choice is 1")
-    
-# elif user_choice == "2":
-#     print("your choice is 2")
-# else:
-#     print("this is an invalid input?!!!")
-#     # if you test this with an invalid input, you can see the game will be terminated
-#     # to avoid this, we have to ask the player again
-#     user_choice = input("choose your drink 1. water 2. tea")
-#     # we can see that the program is not working as required
-#     # to fix this we need to include this conditionl within a loop
-#     # to allow Python to treat the code as a block
-# while True:
-#     choice = input("choose 1 or 2\n 1. Water\n 2. Tea\n")
-#     if choice == "1":
-#         print("your choice is 1")
-    
-#     elif choice == "2":
-#         print("your choice is 2")
-#     else:
-#         print("this is an invalid input?!!!")
-#         choice = input("choose 1 or 2\n 1. Water\n 2. Tea\n")
-        # the code is repeated but another problem rises here 
-        # it takes two inputs to get back to the normal behavior of the program
-        # to fix this, function will be more efficient
-        # check this 
-def check_invalid():
-    choice = input("choose 1 or 2\n 1. Water\n 2. Tea\n")
-    if choice == "1":
-        print("your choice is 1")
-    elif choice == "2":
-        print("your choice is 2")
-    else:
-        print("this is an invalid input?!!!")
-        # instead of reprinting the choice again we can call the function itself
-        check_invalid()
-# do not forget to call the function at the global scope 
-check_invalid()
+# 5. Use randomness in your game
+    # to add randomness, the random module should be imported
+import random
+    # randomness can be anywhere in your game
+    # the goal is to give your player a chance to see 
+    # a new version of the game if s/he likes to play more rounds
+    # to do so, the choice method can be used like this
+places = ["Jungle" ,"desert", "cave"]
+    # since you have more than one place, you may use a random place like this
+print("you find yourself in a " + random.choice(places))
+    # enamies can be randomized too
+enamies = ["monster", "dracula", "ghost", "zombie"]
+print("suddenly a " + random.choice(enamies) + " appeared!")
+    # a good practice is to use the template literals like this
+print(f"suddenly a {random.choice(enamies)} appeared!")
+    # another method to add randomness is to use the randint method
+    # the randint method is used to return a random number between two ranges
+    # for example
+print(random.randint(5, 11))
+    # the randint method can be used in the game scenario like this
+    # the player will reach a situation where he has to fight an enemy
+    # if he choose to fight, you can use the randint method to return
+    # a random hit power like this
+choice = input("choose 1 or 2\n 1. Fight\n 2. run\n")
+if choice == "1":
+    print("you hit your enemy using you gun!")
+    print(f"your hit power is  {random.randint(0, 100)}")
