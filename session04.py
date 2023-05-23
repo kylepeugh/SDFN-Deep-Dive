ca1fb818356f051cb0adb9817d4da176225101f4
Mohammad Allam <uodeeb@gmail.com>
2023-05-23 17:41:03 +0300

    end random

---
+++ session04.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# import the random module 
+import random
+"""This program plays a game of Rock, Paper, Scissors between two Players,
+and reports both Player's scores each round."""
+
+moves = ['rock', 'paper', 'scissors']
+
+"""The Player class is the parent class for all of the Players
+in this game"""
+
+
+class Player:
+    def move(self):
+        return 'rock'
+
+    def learn(self, my_move, their_move):
+        pass
+
+
+def beats(one, two):
+    return ((one == 'rock' and two == 'scissors') or
+            (one == 'scissors' and two == 'paper') or
+            (one == 'paper' and two == 'rock'))
+
+# create a random player subclass to return a random move 
+class RandomPlayer(Player):
+    def move(self):
+        return random.choice(moves)
+
+    def learn(self, my_move, their_move):
+        # test the learn method by print the moves
+        print(f"random player move is: {my_move}||player move is: {their_move}")
+        pass
+
+class Game:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def play_round(self):
+        move1 = self.p1.move()
+        move2 = self.p2.move()
+        print(f"Player 1: {move1}  Player 2: {move2}")
+        self.p1.learn(move1, move2)
+        self.p2.learn(move2, move1)
+
+    def play_game(self):
+        print("Game start!")
+        for round in range(3):
+            print(f"Round {round}:")
+            self.play_round()
+        print("Game over!")
+
+
+if __name__ == '__main__':
+    # change this from player to random player to test 
+    game = Game(Player(), RandomPlayer())
+    game.play_game()
